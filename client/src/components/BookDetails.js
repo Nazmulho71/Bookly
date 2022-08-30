7af4676141525a3781dd5b29a0bdcde262f5b96e
Md Nazmul Hossain <annazmul71@gmail.com>
2022-08-30 19:54:24 +0600

    Add book details page

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,7 +1,71 @@
 import React from "react";
+import "../assets/css/BookDetails.css";
 
-function BookDetails() {
-  return <div className="bookDetails">Book</div>;
+function BookDetails({
+  image,
+  title,
+  subtitle,
+  edition,
+  author,
+  format,
+  category,
+  publisher,
+  publishDate,
+  discountPrice,
+  price,
+  page,
+}) {
+  return (
+    <div className="bookDetails">
+      <img src={image} alt="" width={280} />
+
+      <div className="bookDetails__details">
+        <div>
+          <h3>{title}</h3>
+          <h3>{subtitle}</h3>
+          <p style={{ marginLeft: 5 }}>{edition}</p>
+        </div>
+
+        <hr />
+
+        <div>
+          <span>Author:&nbsp;</span>
+          <p>{author}</p>
+        </div>
+
+        <div>
+          <span>Format:&nbsp;</span>
+          <p>{format}</p>
+        </div>
+
+        <div>
+          <span>Category:&nbsp;</span>
+          <p>{category}</p>
+        </div>
+
+        <div>
+          <span>Publisher:&nbsp;</span>
+          <p>{publisher}</p>
+        </div>
+
+        <div>
+          <span>Published:&nbsp;</span>
+          <p>{publishDate}</p>
+        </div>
+
+        <div>
+          <span>Price:&nbsp;</span>
+          <p style={{ textDecorationLine: "line-through" }}>{discountPrice}</p>
+          <p style={{ color: "#000" }}>&nbsp;{price}</p>
+        </div>
+
+        <div>
+          <span>Pages:&nbsp;</span>
+          <p>{page}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BookDetails;
--- client/src/pages/book.js
+++ client/src/pages/book.js
@@ -1,11 +1,75 @@
 import React from "react";
-import "../assets/css/book.css";
+import Button from "@mui/material/Button";
+import Rating from "@mui/material/Rating";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import BookDetails from "../components/BookDetails";
+import "../assets/css/book.css";
 
 function Book() {
   return (
     <div className="book">
-      <BookDetails />
+      <BookDetails
+        image={
+          "https://m.media-amazon.com/images/P/1118531647.01._SCLZZZZZZZ_SX500_.jpg"
+        }
+        title={"JavaScript and jQuery"}
+        subtitle={"Interactive Front-End Web Development"}
+        edition={"1st Edition"}
+        author={"Jon Duckett"}
+        format={"Paperback"}
+        category={"Programming"}
+        publisher={"Wiley"}
+        publishDate={"December 24, 2013"}
+        discountPrice={"$45"}
+        price={"$39.99"}
+        page={"640"}
+      />
+
+      <Button variant="contained">ORDER NOW</Button>
+
+      <div className="book__description">
+        <div>
+          <h3>Description</h3>
+          <div>
+            <span>2.5 out of 5</span>
+            <Rating
+              name="half-rating-read"
+              defaultValue={2.5}
+              precision={0.5}
+              readOnly
+            />
+          </div>
+        </div>
+        <hr />
+        <p>
+          In JavaScript and jQuery: Interactive Front-End Development,
+          best-selling author Jon Duckett delivers a fully illustrated guide to
+          making your websites more interactive and your interfaces more
+          interesting and intuitive. In the book, you'll explore basic
+          programming concepts that assume no prior knowledge of programming
+          beyond an ability to create a web page using HTML & CSS.{" "}
+          <span>
+            Read more <KeyboardArrowDownIcon />
+          </span>
+        </p>
+      </div>
+
+      <div className="book__review">
+        <h3>Customer reviews</h3>
+        <hr />
+        <div>
+          <AccountCircleIcon />
+          <span>Lilianna</span>
+        </div>
+        <p>
+          I love the illustrations, examples, and simple layout of this book,
+          but...it is outdated. It does not include all the "upgrades" of ES6.
+          For example, I searched the index for the spread operator; not there.
+          It does not cover arrow functions either. So, a good beginner's book?
+          Yes. A reference for middle to advanced Javascript users? Not really.
+        </p>
+      </div>
     </div>
   );
 }
